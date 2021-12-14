d5e438bc4c5bd3fc78ca70924ce8f3dbf6f756db
Enver Morinaj <emorinaj@agroviva.de>
2021-12-14 13:46:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
 <?php
 
-$_GET['cd'] = 'no';
-$GLOBALS['egw_info']['flags'] = [
-    'currentapp'    => 'attendance',
-    'noheader'      => true,
-    'nonavbar'      => true,
-];
-include '../header.inc.php';
-$GLOBALS['egw']->redirect_link('/egroupware/attendance/app/index.php');
-
-# include dirname(__FILE__).'/app/index.php';
+include dirname(__FILE__).'/app/index.php';
