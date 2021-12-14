50c55511bc2c9899be979a41a07202a7b612f90e
Enver Morinaj <emorinaj@agroviva.de>
2021-12-14 15:08:58 +0100

    Update Hooks

--- app/inc/class.attendance_hooks.inc.php
+++ app/inc/class.attendance_hooks.inc.php
@@ -9,9 +9,10 @@
  * @copyright (c) Agroviva GmbH
  */
 
-#include_once __DIR__.'/../api/app.php';
-#use Attendance\Core;
+# Autoload Classes
+require_once __DIR__.'/../classes/autoload.php';
 
+use Attendance\Core;
 use EGroupware\Api\Egw;
 use EGroupware\Api\Link;
 
@@ -22,27 +23,23 @@ class attendance_hooks
     public static function all_hooks($args)
     {
         $appname = 'attendance';
-        $title = lang($GLOBALS['egw_info']['apps'][$appname]['title']);
+        $menu_title = lang($GLOBALS['egw_info']['apps'][$appname]['title']);
         $location = is_array($args) ? $args['location'] : $args;
 
         if ($location == 'sidebox_menu') {
             $file = [
                 'Work Tracker' => Egw::link('/egroupware/attendance/graph/tracker/'),
             ];
-            display_sidebox($appname, $title.'', $file);
+            display_sidebox($appname, $menu_title, $file);
 
             // only users with permission for admin app can see sidebox
             if ($GLOBALS['egw_info']['user']['apps']['admin']) {
 
-                $isManager = true; # Core::isManager($GLOBALS['egw_info']['user']['account_id']);
+                $isManager = Core::isManager($GLOBALS['egw_info']['user']['account_id']);
                 if ($isManager) {
                     $file = [
-                        // 'Work Contracts'               => Egw::link('/index.php', 'menuaction=attendance.attendance_ui.manage&appname=attendance&use_private=1'),
                         'Arbeitsverträge' => Egw::link('/egroupware/attendance/graph/manage/'),
-                        //'Work Contracts 1' => Egw::link('/index.php','menuaction=attendance.attendance_ui.management&appname=attendance&use_private=1'),
-                        //'Work scheduling' => Egw::link('/index.php','menuaction=attendance.attendance_ui.work_schedule&appname=attendance&use_private=1'),
                         'Attendance Time' => Egw::link('/egroupware/attendance/graph/timesheet/'),
-                        // 'Log'             => Egw::link('/index.php', 'menuaction=attendance.attendance_ui.logs&appname=attendance&use_private=1'),
                         'Holidays'        => Egw::link('/egroupware/attendance/graph/holidays/'),
                         'Synchronisation' => Egw::link('/index.php', 'menuaction=attendance.attendance_ui.sync&appname=attendance&use_private=1'),
 
