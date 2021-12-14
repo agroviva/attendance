7d5b4d4c34b416a9ccdba53e96d7067962f9320e
Enver Morinaj <emorinaj@agroviva.de>
2021-12-14 14:25:56 +0100

    Fix Stylesheet

--- app/css/app.css.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-type: text/css', true);
-
-echo
-"
-@import url('/egroupware/attendance/app/css/desktop.css');
-
-@import url('/egroupware/attendance/app/css/mobile.css');
-";
