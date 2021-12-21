e3705ca94b288c1d1d5157999a24929b8f65058d
Enver Morinaj <emorinaj@agroviva.de>
2021-12-21 15:15:13 +0100

    Update create.timesheet.php

--- app/controller/create.timesheet.php
+++ app/controller/create.timesheet.php
@@ -18,10 +18,8 @@ $work = $categories["work"];
 $Query = "INSERT INTO egw_timesheet(ts_start, ts_duration, ts_quantity, ts_title, cat_id, ts_owner, ts_created, ts_modified, ts_modifier) 
 VALUES ('$start', '$duration', '$duration', '$title', '$work', '$user', '$user', '$timeNow', '$user')";
 
-#(new DB($Query));
+(new DB($Query));
 
 echo json_encode(array(
-    $Query, 
-    $_REQUEST,
-    $data
+    "response" => "success"
 ));
