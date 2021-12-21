77a9d8c7a44fd6d5d1f028fa23e11aa5ba9558cf
Enver Morinaj <emorinaj@agroviva.de>
2021-12-21 14:42:48 +0100

    Update create.timesheet.php

--- app/controller/create.timesheet.php
+++ app/controller/create.timesheet.php
@@ -4,5 +4,14 @@ use Attendance\Categories;
 use AgroEgw\DB;
 use Carbon\Carbon;
 
+$categories = Categories::GetCategories();
+$work = $categories["work"];
+$title = "";
+$timeNow = time();
 
-echo json_encode(Categories::GetCategories());
+$Query = "INSERT INTO egw_timesheet(ts_start, ts_duration, ts_quantity, ts_title, cat_id, ts_owner, ts_created, ts_modified, ts_modifier) 
+VALUES ('$start', '$duration', '$duration', '$title', '$work', '$user', '$user', '$timeNow', '$user')";
+
+#(new DB($Query));
+
+echo $Query;
