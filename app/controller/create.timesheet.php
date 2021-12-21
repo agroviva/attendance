c659b8cc9161110eaac9b451a1b979d4a21a1ee2
Enver Morinaj <emorinaj@agroviva.de>
2021-12-21 14:50:38 +0100

    Update create.timesheet.php

--- app/controller/create.timesheet.php
+++ app/controller/create.timesheet.php
@@ -6,13 +6,14 @@ use Carbon\Carbon;
 
 $data = array();
 parse_str($_REQUEST["data"], $data);
+$user = $data["user"];
+$start = Carbon::parse($data["datetime"])->timestamp;
+$duration = $data["duration"] * 60;
+$title = $data["title"];
+$timeNow = time();
 
 $categories = Categories::GetCategories();
 $work = $categories["work"];
-$duration = 1 * 60;
-$title = "Arbeitszeit";
-$timeNow = time();
-
 
 $Query = "INSERT INTO egw_timesheet(ts_start, ts_duration, ts_quantity, ts_title, cat_id, ts_owner, ts_created, ts_modified, ts_modifier) 
 VALUES ('$start', '$duration', '$duration', '$title', '$work', '$user', '$user', '$timeNow', '$user')";
